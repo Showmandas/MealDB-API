0d47f1d51524b445c125b217b766d73aac93506a
Showman_Das <Showmandas3216@gmail.com>
2023-03-04 22:26:10 +0600

    fetch api and display cards

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
-const loadData = () => {
-  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
-    .then(res => res.json())
-    .then(data => displayMeals(data.meals));
-};
+const loadMeal=(mealName)=>{
+  const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`
+  fetch(url)
+  .then(res=>res.json())
+  .then(data=>displayMeals(data.meals))
+}
 
-const displayMeals = datas => {
-  console.log(datas);
-  const mealContainer = document.getElementById("meal-container");
-  datas.forEach(meal => {
-    console.log(meal);
-    // const {strMealThumb,strMeal,strInstructions}=meal;
-    const mealdiv = document.createElement("div");
-    mealdiv.classList.add("col");
-    mealdiv.innerHTML = `
+const displayMeals=(meals=>{
+  console.log(meals);
+  const mealContainer=document.getElementById('meal-container');
+  mealContainer.innerHTML='';
+
+  meals.forEach(meal=>{
+    const {strMealThumb,strMeal,strInstructions,idMeal}=meal;
+    // console.log(meal);
+    const div=document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML=`
     <div class="card h-100">
-                    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">${meal.strMeal}</h5>
-                      <p class="card-text">${meal.strInstructions}</p>
-                    </div>
-                  </div>
+        <img src="${strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${strMeal}</h5>
+          <p class="card-text">${strInstructions.slice(0,100)}</p>
+          <button class='btn btn-warning' id="detailBtn" onclick=detailShow(${idMeal})>Details</button>
+        </div>
+      </div>
     `;
-    mealContainer.appendChild(mealdiv)
-  });
-};
+    mealContainer.appendChild(div);
+  })
 
-console.log(loadData());
+})
+document.getElementById('searchBtn').addEventListener('click',function(){
+  const searchMeal=document.getElementById('searchMeal');
+  const meal=searchMeal.value;
+  loadMeal(meal);
+  searchMeal.value=''
+})
+// loadMeal();
